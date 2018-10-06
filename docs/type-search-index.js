5558068914eb42eb88322f2751d120e513828da6
drtnf <tim.french@uwa.edu.au>
2018-10-06 12:38:30 +0800

    ignore documentation directory

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"hanabAI","l":"Action"},{"p":"hanabAI","l":"ActionType"},{"p":"hanabAI","l":"Agent"},{"p":"hanabAI","l":"Card"},{"p":"hanabAI","l":"Colour"},{"p":"hanabAI","l":"Hanabi"},{"p":"hanabAI","l":"IllegalActionException"},{"p":"hanabAI","l":"State"}]
